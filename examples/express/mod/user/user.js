cfcb7ca97521dec28b72f18147e30625ed38c599
cz-Michael <zchning@gmail.com>
2016-01-21 16:28:26 -0500

    wrap user into closure function

--- examples/express/mod/user/user.js
+++ examples/express/mod/user/user.js
@@ -1,33 +1,38 @@
-var CZ = require('cz-actor');
+(function() {
+    var CZ = require('cz-actor');
 
 
-var User = (function() {
-    function User() {
-        this.actor = CZ.Actor({
-            id: 'userActor',
-            process: function(sender, message) {
-                console.log("userActor get a message from ", sender, message);
-            }
-        });
-    }
+    var User = (function() {
+        function User() {
+            this.actor = CZ.Actor({
+                id: 'userActor',
+                process: function(sender, message) {
+                    console.log("userActor get a message from ", sender, message);
+                }
+            });
+        }
 
-    User.prototype.start = function() {
-        this.actor.send('apiActor', {
-            type: 'route',
-            request: {
-                method: 'GET',
-                url: '/users',
-                handlers: [
-                    function(req, res, next) {
-                        res.json({success: true, result: 'All Users'});
-                    }
-                ]
-            }
-        });
-    };
+        User.prototype.start = function() {
+            this.actor.send('apiActor', {
+                type: 'route',
+                request: {
+                    method: 'GET',
+                    url: '/users',
+                    handlers: [
+                        function(req, res, next) {
+                            res.json({success: true, result: 'All Users'});
+                        }
+                    ]
+                }
+            });
+        };
 
-    return User;
-})();
+        return User;
+    })();
 
-module.exports = new User();
+    module.exports = new User();
+
+}).call(this);
+
+
 
