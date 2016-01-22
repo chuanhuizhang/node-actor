25f9998caf5bb8f2aa773574e7261fc772c7ff5b
cz-Michael <zchning@gmail.com>
2016-01-22 14:07:35 -0500

    support receiver return after asynchronous call

--- examples/test.js
+++ examples/test.js
@@ -2,17 +2,25 @@ var CZ = require('../src/cz');
 
 var actorOne = CZ.Actor({
     id: 'actorOne',
-    process: function(sender, message) {
+    process: function(sender, message, promise) {
         console.log("actorOne get a message from ", sender, message);
+        setTimeout(function() {
+            promise.resolve('From 1');
+        }, 3000);
     }
 });
 
 var actorTwo = CZ.Actor({
     id: 'actorTwo',
-    process: function(sender, message) {
+    process: function(sender, message, promise) {
         console.log("actorTwo get a message from ", sender, message);
+        promise.resolve('From 2');
     }
 });
 
 
-actorTwo.send('actorOne', {msg: 'Message from actorTwo'});
+actorTwo.send('actorOne', {msg: 'Message from actorTwo'}).then(function(result){
+    console.log(result);
+}, function(err) {
+    console.log(err);
+});
